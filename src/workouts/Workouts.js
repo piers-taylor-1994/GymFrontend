c4da3fc84e6d46ca135c67e5ccda182488348925
Piers Taylor <piers.taylorr@gmail.com>
2023-06-07 16:31:59 +0100

    Added checkbox and new list functionality to workouts page

--- src/workouts/Workouts.js
+++ src/workouts/Workouts.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { GetExercises } from "./Data";
+import './workouts.scss';
 
 const MuscleGroup = {
     0: "Shoulders",
@@ -19,23 +20,42 @@ function Workouts(props) {
         })
     }, [])
 
-    const row = (e) => {
+    const onCheck = (e, exercise) => {
+        if (e.target.checked) {
+            setSelectedExercises((se) => {
+                return [...se, exercise];
+            })
+        }
+        else {
+            setSelectedExercises(selectedExercises.filter((ex) => ex.id !== exercise.id));
+        }
+    }
+
+    const row = (exercise) => {
         return (
-            <div key={e.id}>
-                <p>{e.id}</p>
-                <p>{MuscleGroup[e.muscleGroup]}</p>
-                <p>{e.name}</p>
-                <p>{e.description}</p>
-                <input type="checkbox" />
+            <div key={exercise.id} className="rows">
+                <p>{MuscleGroup[exercise.muscleGroup]}</p>
+                <p>{exercise.name}</p>
+                <p>{exercise.description}</p>
+                <input type="checkbox" checked={selectedExercises.includes(exercise)} onChange={(e) => onCheck(e, exercise)}/>
             </div>
         )
     }
 
     const exercisesDisplay = exercises.map(e => row(e));
+    const selectedExercisesDisplay = selectedExercises.map(e => row(e));
+
+    const submit = selectedExercises.length > 0 ? <button>Submit</button> : <></>;
 
     return (
-        <div>
-            {exercisesDisplay}
+        <div className="workouts">
+            <div className="workouts-container top">
+                {exercisesDisplay}
+            </div>
+            <div className="workouts-container">
+                {selectedExercisesDisplay}
+            </div>
+            {submit}
         </div>
     )
 }
