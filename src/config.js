241884e983623420d9e84910deac20c053321af4
Piers Taylor <piers.taylorr@gmail.com>
2023-06-20 16:42:30 +0100

    Added azure API to config + removed authContext from homepage (unused)

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 let config = {
-    host: process.env.NODE_ENV === 'development' ? "https://localhost:7078/" : "https://api.co.uk"
+    host: process.env.NODE_ENV === 'development' ? "https://localhost:7078/" : "https://gymapp-api.azurewebsites.net/"
 }
 
 export default config;
--- src/homepage/Homepage.js
+++ src/homepage/Homepage.js
@@ -1,12 +1,6 @@
-import { useContext } from "react"
-import { AuthContext } from "../auth/Auth"
 import { Format } from "../dates";
 
 function Homepage(props) {
-    const authContext = useContext(AuthContext);
-    const user = authContext.user();
-    console.log(user);
-
     return (
         <div className="homepage">
             <h1>{Format(new Date()).day}</h1>
