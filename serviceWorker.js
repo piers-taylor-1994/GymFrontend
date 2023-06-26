3d5fd206d8555ac4020e7b28691e3eb73f9a8107
Piers Taylor <piers.taylor@medway.gov.uk>
2023-06-26 12:53:51 +0100

    Updates

---
+++ serviceWorker.js
@@ -0,0 +1,97 @@
+const cacheName = "gymapp-cache-v1";
+
+const currentCaches = {
+    data: cacheName + '-data'
+}
+
+let jwt = '';
+
+const hasHeader = function (headers, pattern) {
+    return headers.has('content-type') && headers.get('content-type').match(pattern);
+}
+
+self.addEventListener('install', event => {
+    console.log('[Service Worker] Install');
+    console.log("%PUBLIC_URL%/logo192.png");
+    // precache our static resources for this build of the app here
+
+})
+
+self.addEventListener('message', event => {
+    if(event.data && event.data.type === "STORE-TOKEN"){
+        jwt = event.data.token;
+    }
+
+    if(event.data && event.data.type === "CLEAR-TOKEN"){
+        jwt = '';
+        clients.matchAll().then(all => {
+            if(all[0] && all[0].focused){
+                all[0].navigate(all[0].url);
+            }
+        })
+    }
+
+    if(event.data && event.data.type === "NOTIFICATION"){
+        let data = event.data.notification;
+        if(Notification.permission !== 'granted'){
+            return;
+        }
+
+        registration.showNotification(data.content);
+    }
+})
+
+self.addEventListener('activate', event => {
+    var expectedCacheNames = new Set(Object.values(currentCaches));
+
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (expectedCacheNames.has(cacheName) == false)
+                        return caches.delete(cacheName);
+                })
+            );
+        })
+    );
+    event.waitUntil(self.clients.claim());
+})
+
+const destinations = ['', 'font', 'image'];
+
+self.addEventListener('fetch', event => {
+    if(event.request.url.endsWith('auth/logon')){
+        return fetch(event.request);
+    }
+    if(destinations.includes(event.request.destination) == false){
+        return fetch(event.request);
+    }
+
+    let cache = currentCaches.data;
+    if (event.request.destination === 'font')
+        cache = currentCaches.font;
+
+    event.respondWith(
+        caches.open(cache).then(cache => {
+            return cache.match(event.request).then(response => {
+                if (response) return response;
+
+                if(event.request.url.indexOf("cdn.jsdelivr.net/npm") > 0){
+                    return fetch(event.request);
+                }
+                
+                if(jwt !== ''){
+                    let headers = new Headers(event.request.headers);
+                    if(headers.get("Authorization") === null)
+                        headers.append('Authorization', 'Bearer ' + jwt);
+                    return fetch(event.request, {
+                        mode: 'cors',
+                        headers: headers
+                    });
+                }else{
+                    return fetch(event.request);
+                }
+            })
+        })
+    );
+})
