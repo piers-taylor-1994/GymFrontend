33913f6af02551fb499c0609c87c29ad89dfc8f1
Piers Taylor <piers.taylorr@gmail.com>
2023-06-30 15:42:13 +0100

    Removed homepage routing as may be bugged the app

--- src/homepage/Homepage.js
+++ src/homepage/Homepage.js
@@ -1,13 +1,6 @@
-import { useNavigate } from "react-router-dom";
 import { Format } from "../layout/dates";
-import { useEffect } from "react";
 
 function Homepage(props) {
-    const navigate = useNavigate();
-
-    useEffect(() => {
-        navigate("/workouts");
-    }, [navigate])
     return (
         <div className="homepage content">
             <h1>{Format(new Date()).day}</h1>
